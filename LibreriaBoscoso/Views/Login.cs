6d9fe2fdc32c90b26fa285ce782cb0d21564c8b7
Luca04AM <luiscarlosnet9cr@gmail.com>
2025-02-12 15:41:49 -0600

    Ventanas inicio

--- LibreriaBoscoso/Views/Login.cs
+++ LibreriaBoscoso/Views/Login.cs
@@ -10,26 +10,18 @@ using System.Windows.Forms;
 
 namespace LibreriaBoscoso.Views
 {
-    public partial class Inicio : Form
+    public partial class Login : Form
     {
-        public Inicio()
+        public Login()
         {
             InitializeComponent();
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void panel2_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
+       
     }
 }
